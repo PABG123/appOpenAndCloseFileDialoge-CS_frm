8b0b9b0ec9fc483d0347b1763aa5039b82f824a9
Alonso Ballesteros <78063880+PABG123@users.noreply.github.com>
2021-10-26 08:02:10 -0500

    Add project files.

---
+++ appOpenAndCloseFileDialoge-CS_frm/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace appOpenAndCloseFileDialoge_CS_frm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (StreamReader Reader = new StreamReader(@"F:\Test.txt"))
+            {
+                while (!Reader.EndOfStream)
+                {
+                    richTextBox1.AppendText(Reader.ReadLine());
+                }
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter Writer = new StreamWriter(@"F:\Test.txt"))
+            {
+                Writer.WriteLine(richTextBox1.Text);
+            }
+        }
+    }
+}
